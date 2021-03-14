8b87cf40efe881aca70ffd6edd9d48fc6de1d33f
Rafael Almeida <debemrafael@gmail.com>
2021-03-14 02:16:25 -0300

    :children_crossing: Read ASM file from the command line. Improve main.py

--- main.py
+++ main.py
@@ -3,22 +3,30 @@
 # ---- Author: Rafael Almeida de Bem
 # ---- Created on: 09/03/2021 (DD/MM/YYYY)
 # ---- Modified on: 13/03/2021
+# ----              14/03/2021
 # -----------------------------------------------------------------------------
 
 from __future__ import annotations
 
 import pathlib
+import sys
 import tkinter as tk
 
-from virtual_machine import VirtualMachine
+from source.vm.virtual_machine import VirtualMachine
+
+try:
+    file = sys.argv[1]
+except:
+    file = 'example_programs/p2.asm'
 
 root = tk.Tk()
+root.protocol('WM_DELETE_WINDOW', lambda: (root.quit(), root.destroy()))
 root.resizable(False, False)
 text = tk.Text(root, width=120, height=80)
 # text = None
 
 vm = VirtualMachine(mem_size=512, tk=text)
-vm.load_from_file(pathlib.Path('example_programs/p3.asm'))
+vm.load_from_file(pathlib.Path(file))
 vm.start()
 
 if text:
