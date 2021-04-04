13abab221cf5858a735b398fa530bb5781aae59f
Rafael Almeida <debemrafael@gmail.com>
2021-04-04 17:16:45 -0300

    :white_check_mark: :recycle: Support IWord #11

--- test/asm_test.py
+++ test/asm_test.py
@@ -24,7 +24,7 @@ class AssemblyTest(unittest.TestCase):
         # Test Fibonacci sequence
         for address, result in [(50, 0), (51, 1), (58, 21), (59, 34)]:
             with self.subTest(address=address, result=result):
-                self.assertEqual(self.vm.memory.access(address).execute(), result)
+                self.assertEqual(self.vm.memory.access(address).command.execute(), result)
 
     def test_p2(self):
         """
@@ -35,13 +35,13 @@ class AssemblyTest(unittest.TestCase):
         self.vm.start()
         self.vm.join()
 
-        amount = self.vm.memory._inner_memory[1].p
-        start_address = self.vm.memory._inner_memory[2].p
+        amount = self.vm.memory._inner_memory[1].command.p
+        start_address = self.vm.memory._inner_memory[2].command.p
 
         for address, result in [(start_address, amount),
                                 *zip([x + start_address + 1 for x in range(amount)], fibonacci(length=amount))]:
             with self.subTest(address=address, result=result):
-                self.assertEqual(self.vm.memory.access(address).execute(), result)
+                self.assertEqual(self.vm.memory.access(address).command.execute(), result)
 
     @parameterized.expand([
         (3,), (5,), (20,)
@@ -61,12 +61,12 @@ class AssemblyTest(unittest.TestCase):
             self.vm.start()
             self.vm.join()
 
-        start_address = self.vm.memory._inner_memory[2].p
+        start_address = self.vm.memory._inner_memory[2].command.p
 
         for address, result in [(start_address, fib_amount),
                                 *zip([x + start_address + 1 for x in range(fib_amount)], fibonacci(length=fib_amount))]:
             with self.subTest(address=address, result=result):
-                self.assertEqual(self.vm.memory.access(address).execute(), result)
+                self.assertEqual(self.vm.memory.access(address).command.execute(), result)
 
     def test_p3(self):
         """
@@ -82,14 +82,14 @@ class AssemblyTest(unittest.TestCase):
         self.vm.start()
         self.vm.join()
 
-        number = self.vm.memory._inner_memory[3].p
+        number = self.vm.memory._inner_memory[3].command.p
 
         if number < 0:
             factorial = -1
         else:
             factorial = math.factorial(number)
 
-        self.assertEqual(self.vm.memory.access(50).execute(), factorial)
+        self.assertEqual(self.vm.memory.access(50).command.execute(), factorial)
 
     @parameterized.expand([
         (5,), (6,), (-2,), (0,)
@@ -114,7 +114,7 @@ class AssemblyTest(unittest.TestCase):
         else:
             factorial = math.factorial(number)
 
-        self.assertEqual(self.vm.memory.access(50).execute(), factorial)
+        self.assertEqual(self.vm.memory.access(50).command.execute(), factorial)
 
     def test_p4(self):
         """
@@ -123,7 +123,9 @@ class AssemblyTest(unittest.TestCase):
         Bubble sorts an array
         """
 
-        literal_array = [73, 29, 8, 82, 199, 62, 164, 182, 29, 197, 38, 2, 186, 192, 35, 18, 122, 138, 181, 195, 86, 174, 75, 135, 7, 12, 33, 67, 62, 133, 55, 104, 78, 84, 91, 121, 73, 178, 117, 109, 4, 163, 11, 182, 54, 77, 107, 197, 81, 100]
+        literal_array = [73, 29, 8, 82, 199, 62, 164, 182, 29, 197, 38, 2, 186, 192, 35, 18, 122, 138, 181, 195, 86,
+                         174, 75, 135, 7, 12, 33, 67, 62, 133, 55, 104, 78, 84, 91, 121, 73, 178, 117, 109, 4, 163, 11,
+                         182, 54, 77, 107, 197, 81, 100]
         literal_array.sort()
 
         self.vm.load_from_file(Path('example_programs/p4.asm'))
@@ -131,7 +133,7 @@ class AssemblyTest(unittest.TestCase):
         self.vm.start()
         self.vm.join()
 
-        self.assertEqual(literal_array, [self.vm.memory.access(i).execute() for i in range(300, 350)])
+        self.assertEqual(literal_array, [self.vm.memory.access(i).command.execute() for i in range(300, 350)])
 
 
 if __name__ == '__main__':
