20c0db7eb5db7b559e927c617a61217e074d86ab
Rafael Almeida <debemrafael@gmail.com>
2021-05-02 19:34:42 -0300

    :bug: Remove premature loop breakage

--- source/word/word.py
+++ source/word/word.py
@@ -1,20 +1,12 @@
 from abc import ABC
 from typing import Any
 
+from source.command.command import BaseCommand
 
 class IWord(ABC):
     command: Any
 
 
-class Word(IWord):
+class Word(IWord, BaseCommand):
     def __init__(self, command=None):
         self.command = command
-
-    def dump(self): return self.command.dump()
-
-    @property
-    def original(self): return self.command.original
-
-    def set_instance_params(self, **kwargs): self.command.set_instance_params(**kwargs)
-
-    def execute(self): self.command.execute()
